b2d1d6ee96b0edf345f0afa07795ba96331a99c7
desarrollocorrales <desarrollo@loscorrales.com.mx>
2017-04-28 12:36:32 -0500

    Guardar

--- SegCoWebServices/EntidadEtiquetas.cs
+++ SegCoWebServices/EntidadEtiquetas.cs
@@ -15,9 +15,28 @@ namespace SegCoWebServices
             List<string> lstNumerosDeEtiquetas = 
                 NumerosDeEtiquetas.Split(Environment.NewLine.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
 
+            //Eliminar duplicados
+            lstNumerosDeEtiquetas = lstNumerosDeEtiquetas.Distinct().ToList();
+
             var listEtiquetasEncontradas = (from buscaEtiqueta in Contexto.etiquetas
                                             where (lstNumerosDeEtiquetas.Contains(buscaEtiqueta.numero_etiqueta))
                                             select buscaEtiqueta).ToList();
+
+            var listEtiquetasEnPaquetes = (from buscaEtiqueta in Contexto.paquetes
+                                            where (lstNumerosDeEtiquetas.Contains(buscaEtiqueta.numero_etiqueta))
+                                            select buscaEtiqueta).ToList();
+
+            foreach (paquetes p in listEtiquetasEnPaquetes)
+            {
+                foreach (paquetes_det pd in p.paquetes_det)
+                {
+                    if (pd.etiquetas.estatus != "B")
+                    {
+                        pd.etiquetas.id_usuario = Convert.ToInt64(p.numero_etiqueta);
+                        listEtiquetasEncontradas.Add(pd.etiquetas);
+                    }
+                }
+            }
 
             EtiquetasGrid Etiqueta;
             List<EtiquetasGrid> Resultado = new List<EtiquetasGrid>();
@@ -25,12 +44,19 @@ namespace SegCoWebServices
             {
                 Etiqueta = new EtiquetasGrid();
                 Etiqueta.NumeroDeEtiqueta = Etiq.numero_etiqueta;
+                Etiqueta.TipoEtiqueta = Etiq.tipo_etiqueta;
+                Etiqueta.IdProveedor = Etiq.id_proveedor;
+                Etiqueta.IdLote = Etiq.id_lote;
+                Etiqueta.IdPedido = Etiq.id_pedido;
+                Etiqueta.Clave = Etiq.clave_articulo;
                 Etiqueta.ClaveNombre = Etiq.articulos.clave + " - " + Etiq.articulos.articulo;
                 Etiqueta.FechaDeEmpaque = Etiq.fecha_empaque;
                 Etiqueta.FechaDeCaducidad = Etiq.fecha_caducidad;
                 Etiqueta.Cantidad = Etiq.cantidad;
                 Etiqueta.Unidad = Etiq.unidad;
-
+                Etiqueta.Piezas = Etiq.piezas;
+                Etiqueta.FechaDeRecepcion = Etiq.fecha_recepcion;
+                Etiqueta.EtiquetaTarima = Etiq.id_usuario.ToString();
                 Resultado.Add(Etiqueta);
             }
 
--- SegCoWebServices/Models/EtiquetasGrid.cs
+++ SegCoWebServices/Models/EtiquetasGrid.cs
@@ -8,10 +8,18 @@ namespace SegCoWebServices.Models
     public class EtiquetasGrid
     {
         public string NumeroDeEtiqueta { set; get; }
+        public string TipoEtiqueta { set; get; }
+        public int? IdProveedor { set; get; }
+        public long? IdLote { set; get; }
+        public long? IdPedido { set; get; }
+        public string Clave { set; get; }
         public string ClaveNombre { set; get; }
         public DateTime FechaDeEmpaque { set; get; }
         public DateTime FechaDeCaducidad { set; get; }
         public Decimal Cantidad { set; get; }
         public string Unidad { set; get; }
+        public long? Piezas { set; get; }
+        public DateTime? FechaDeRecepcion { get; set; }
+        public string EtiquetaTarima { set; get; }
     }
 }
